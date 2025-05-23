f07b4295428c42313196c8ec31fc9c9312648adb
MatiasJasban <matiasjasban98@gmail.com>
2025-05-22 23:25:38 -0400

    avances en controller

--- src/main/java/cl/duocuc/MicroServicioSucursal/controlador/SucursalControlador.java
+++ src/main/java/cl/duocuc/MicroServicioSucursal/controlador/SucursalControlador.java
@@ -1,9 +1,18 @@
 package cl.duocuc.MicroServicioSucursal.controlador;
 
+import cl.duocuc.MicroServicioSucursal.modelo.ModelSucursal;
 import cl.duocuc.MicroServicioSucursal.repositorio.SucursalRepository;
+import cl.duocuc.MicroServicioSucursal.servicio.SucursalServicio;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sucursal")
@@ -11,7 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class SucursalControlador {
 
     @Autowired
-    private SucursalRepository sucursalrepository;
+    private SucursalServicio sucursalservicio;
 
-   // @PostMapping("/create")
+   @PostMapping("/create")
+    public ResponseEntity<ModelSucursal> createSucursal(@RequestBody ModelSucursal sucursal){
+
+       try{
+           ModelSucursal sucursal_creada=sucursalservicio.createSucursal(sucursal);
+           return ResponseEntity.status(HttpStatus.CREATED).body(sucursal_creada);
+
+       }catch(Exception e){
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+       }
+
+   }
+
+    public ResponseEntity<List<ModelSucursal>>getAllSucursales(){
+       try{
+          List<ModelSucursal> sucursales_encontradas=sucursalservicio.getAll();
+          return ResponseEntity.status(HttpStatus.FOUND).body(sucursales_encontradas);
+
+       }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+       }
+
+    }
+
+
+
+
 }
--- src/main/java/cl/duocuc/MicroServicioSucursal/servicio/SucursalServicio.java
+++ src/main/java/cl/duocuc/MicroServicioSucursal/servicio/SucursalServicio.java
@@ -8,6 +8,9 @@ import cl.duocuc.MicroServicioSucursal.repositorio.SucursalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class SucursalServicio {
 
@@ -17,7 +20,26 @@ public class SucursalServicio {
     public ModelSucursal createSucursal(ModelSucursal sucursal){
         return sucursalrepository.save(sucursal);
     }
-    
+
+
+    public Optional<ModelSucursal> findbyId(Long id){
+        return sucursalrepository.findById(id);
+    }
+
+    public List<ModelSucursal> getAll(){
+        return sucursalrepository.findAll();
+    }
+
+
+    public List<ModelSucursal> deleteUser(Long id){
+        if(sucursalrepository.existsById(id)){
+            sucursalrepository.deleteById(id);
+        }
+            return sucursalrepository.findAll();
+        }
+
+
+
 
     public  ModelSucursal updateSucursal(Long id,ModelSucursal udaptesucursal){
         return sucursalrepository.findById(id).map(sucursal -> {;
